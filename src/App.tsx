3174f288e504fdd0620f7ea7175795ae56522dc6
“xishuianfeng” <xishuianfeng@gmail.com>
2023-03-01 01:48:42 +0800

    完成转换成mp4格式

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,26 @@ import { useRef } from "react"
 import './App.scss'
 
 const ffmpeg = createFFmpeg()
-ffmpeg.load().then(()=>{
-  console.log('加载完成');
+ffmpeg.load().then(() => {
+  console.log("加载完成")
 })
+
+const transformWebm = async (blob:Blob) => {
+  return blob
+    .arrayBuffer()
+    .then((buffer)=>{
+      const arrayBuffer = new Uint8Array(buffer)
+      //我们有ArrayBuffer   需要Uint8Array
+      console.log("开始转换");     
+      ffmpeg.FS('writeFile',"test.webm",arrayBuffer)
+      return ffmpeg.run("-i","test.webm","output.mp4")
+    })
+    .then(()=>{
+      console.log("转换完成");      
+      const array = ffmpeg.FS("readFile","output.mp4")
+      return new Blob([array])
+    })
+}
 
 function App() {
   const videoRef = useRef<HTMLVideoElement>(null!)
@@ -35,20 +52,22 @@ function App() {
     recorderRef.current.stop()
   }
 
-  const onTransfrom = () => {
-
+  const transformedBlob = useRef<Blob>(null!)
+  const onTransfrom = async () => {
+    //转换格式 webm 变成 mp4
+    const videoBlob = new Blob(blobs.current)
+    const mp4Blob = await transformWebm(videoBlob)
+    transformedBlob.current = mp4Blob
   }
 
   const onDownload = ()=> {
-    const videoBlob = new Blob(blobs.current, {
-      type: 'video/webm'
-    })
+    const videoBlob = transformedBlob.current
     const url = URL.createObjectURL(videoBlob)
 
     //下载视频
     const link = document.createElement('a')
     link.href = url
-    link.download = 'test.webm'
+    link.download = 'test.mp4'
     document.body.appendChild(link)
     link.style.display = 'none'
     link.click()
@@ -61,8 +80,8 @@ function App() {
     <div className="App" style={{ margin: 100 }}>
       <button onClick={onStart}>开始录制</button>
       <button onClick={onEnd}>结束录制</button>
-      <button onClick={onTransfrom}>转换格式</button>
-      <button onClick={onDownload}>下载</button>
+      <button onClick={onTransfrom}>转换成MP4格式</button>
+      <button onClick={onDownload}>下载MP4视频文件</button>
       <video ref={videoRef} autoPlay></video>
     </div>
   )
